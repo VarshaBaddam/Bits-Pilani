56d7ba12ab5dc8181cad03eec3431190b4aac986
VarshaBaddam <66900222+VarshaBaddam@users.noreply.github.com>
2020-06-20 14:02:30 +0530

    Add files via upload

---
+++ Test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+############################################
+#Name : Varsha Baddam
+Title : Test1
+Description  :  This is the first script
+################
+echo "welcome to Bits-Pilani" 
+echo "Lets start scripting"
+echo -e "WELCOME"
